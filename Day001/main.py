347c1cc3b7d1c847eeae90dd4024bc56fc7f25a5
Ramon <ramonoliveirasantos170@gmail.com>
2026-07-21 16:31:21 -0300

    Day 1
    Conclusão do dia 1

--- Day001/main.py
+++ Day001/main.py
@@ -1,1 +1,12 @@
-print("Hello World\nHello World")
+print("Hello World\nHello World!")
+print("Hello" + " " + "Sidney")
+
+# print("Hello" + " " + input("What is your name? ") + "!")
+
+#print(len(input("What is your name?: ")))
+
+username = input("What is your name?: ")
+length = len(username)
+
+print(length)
+
