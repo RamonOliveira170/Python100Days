dbf43563966d288865e9f39904f472ebd3675bbd
Ramon <ramonoliveirasantos170@gmail.com>
2026-08-21 19:29:19 -0300

    Day 21
    Conclusão do dia 21

--- Day020/main.py
+++ Day020/main.py
@@ -1,5 +1,7 @@
 import turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 my_screen = turtle.Screen()
@@ -9,6 +11,8 @@ my_screen.title("My Snake Game")
 my_screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 my_screen.listen()
 my_screen.onkey(snake.up,"Up" )
@@ -21,5 +25,20 @@ while game_is_on:
     my_screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # detect collision with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        scoreboard.game_over()
+        game_is_on = False
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
 my_screen.exitonclick()
