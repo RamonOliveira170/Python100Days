fce6212f4ca06c3bf9100ce43cf9dde530b461fc
Ramon <ramonoliveirasantos170@gmail.com>
2026-09-05 21:18:55 -0300

    Day 30
    Conclusão do dia 30

--- Day026/NATO_alphabet_project/main.py
+++ Day026/NATO_alphabet_project/main.py
@@ -1,8 +1,17 @@
 import pandas
 
 alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-user_word = input("Enter a word: ").upper()
 dictionary = {row.letter: row.code for (index, row) in alphabet.iterrows()}
-output_list = [dictionary[letter] for letter in user_word]
 
-print(output_list)
+
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper().replace(" ", "")
+    try:
+        output_list = [dictionary[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
--- Day029/main.py
+++ Day029/main.py
@@ -2,6 +2,8 @@ import tkinter
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -30,9 +32,25 @@ def generate_password():
     password_input.insert(0, password)
     pyperclip.copy(password)
 
+def search_account():
+    website = website_input.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data_accounts = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Alert", message="No Data file found.")
+    else:
+        try:
+            if data_accounts[website]:
+                messagebox.showinfo(title=website,
+                                    message=f"Email: {data_accounts[website]["email"]}\n"
+                                            f"Password: {data_accounts[website]["password"]}")
+        except KeyError:
+            messagebox.showinfo(title="Alert",
+                                message=f"No details for {website} exists.!")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
+def save_account():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
@@ -45,9 +63,19 @@ def save():
                                                                   f"Password: {password}\n"
                                                                   f"It is ok to save?")
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                # clear inputs
+            json_dictionary = {website: {"email": email,
+                                         "password": password}}
+            try:
+                with open('data.json', 'r') as data_file:
+                    data = json.load(data_file)
+            except(FileNotFoundError, json.decoder.JSONDecodeError):
+                with open('data.json', 'w') as data_file:
+                    json.dump(json_dictionary, data_file, indent=4)
+            else:
+                data.update(json_dictionary)
+                with open('data.json', 'w') as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_input.delete(0, 999)
                 password_input.delete(0, 999)
 
@@ -65,9 +93,12 @@ canvas.grid(column=1, row=0)
 website_label = tkinter.Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_input = tkinter.Entry(width=52)
+website_input = tkinter.Entry(width=34)
 website_input.grid(column=1, row=1, columnspan=2, sticky="w")
 website_input.focus()
+
+search_button = tkinter.Button(text="Search", command=search_account)
+search_button.grid(column=2, row=1, sticky="wens")
 
 email_label = tkinter.Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
@@ -85,7 +116,7 @@ password_input.grid(column=1, row=3)
 password_button = tkinter.Button(text="Generate Password", command=generate_password)
 password_button.grid(column=2, row=3, sticky="w")
 
-add_button = tkinter.Button(text="Add", width=58, command=save)
+add_button = tkinter.Button(text="Add", width=58, command=save_account)
 add_button.grid(columns=1, row=4, columnspan=3)
 
 window.mainloop()
